2faf1783f95b71566e41063b9564cc2796001c2f
danieleangeletti <angelettidaniele@gmail.com>
2024-03-15 16:41:35 +0100

    impostazione chiamata axios in post & parametri

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -5,7 +5,25 @@ export default {
   data() {
     return {
       mainTitle: "CONTACT PAGE",
+      name: "",
+      email: "",
+      message: "",
+      accepted: false,
     };
+  },
+  methods: {
+    sendMessage() {
+      axios
+        .post("http://127.0.0.1:8000/api/contacts", {
+          name: this.name,
+          email: this.email,
+          message: this.message,
+          accepted: this.accepted,
+        })
+        .then((res) => {
+          console.log(res.data);
+        });
+    },
   },
 };
 </script>
@@ -14,10 +32,11 @@ export default {
   <main>
     <div class="container">
       <h1 class="text-center mt-1 mb-1">{{ mainTitle }}</h1>
-      <form class="mb-3">
+      <form class="mb-3" method="POST" @submit.prevent="sendMessage()">
         <div class="mb-3">
           <label for="name" class="form-label">Name<span>*</span></label>
           <input
+            v-model="name"
             type="text"
             class="form-control"
             id="name"
@@ -31,6 +50,7 @@ export default {
             >Email address<span>*</span></label
           >
           <input
+            v-model="email"
             type="email"
             class="form-control"
             id="email"
@@ -41,6 +61,7 @@ export default {
         <div class="mb-3">
           <label for="message" class="form-label">Message<span>*</span></label>
           <textarea
+            v-model="message"
             class="form-control"
             id="message"
             name="message"
@@ -49,9 +70,19 @@ export default {
           ></textarea>
         </div>
         <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1"
-            >Check me out<span>*</span></label
+          <input
+            v-model="accepted"
+            type="checkbox"
+            class="form-check-input"
+            id="accept"
+            name="accept"
+            value="1"
+            required
+          />
+          <label class="form-check-label" for="accept"
+            ><span>*</span>I have read and accept the
+            <a href="#" target="_blank">terms of use</a> and the
+            <a href="#" target="_blank">privacy policy</a></label
           >
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
