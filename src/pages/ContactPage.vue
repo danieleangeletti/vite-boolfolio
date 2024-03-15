63c918ecf504558983d4dc8f5c77bdc4efaa1327
danieleangeletti <angelettidaniele@gmail.com>
2024-03-15 20:09:44 +0100

    ADD schermata messaggio inviato correttamente

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -9,6 +9,7 @@ export default {
       email: "",
       message: "",
       accepted: false,
+      messageSent: false,
     };
   },
   methods: {
@@ -22,6 +23,10 @@ export default {
         })
         .then((res) => {
           console.log(res.data);
+
+          if (res.data.success) {
+            this.messageSent = true;
+          }
         })
         .catch((err) => {
           alert("ERROR: invalid data");
@@ -35,64 +40,73 @@ export default {
   <main>
     <div class="container">
       <h1 class="text-center mt-1 mb-1">{{ mainTitle }}</h1>
-      <form class="mb-3" method="POST" @submit.prevent="sendMessage()">
-        <div class="mb-3">
-          <label for="name" class="form-label">Name<span>*</span></label>
-          <input
-            v-model="name"
-            type="text"
-            class="form-control"
-            id="name"
-            name="name"
-            placeholder="Insert name"
-            required
-          />
-        </div>
-        <div class="mb-3">
-          <label for="email" class="form-label"
-            >Email address<span>*</span></label
-          >
-          <input
-            v-model="email"
-            type="email"
-            class="form-control"
-            id="email"
-            name="email"
-            required
-          />
-        </div>
-        <div class="mb-3">
-          <label for="message" class="form-label">Message<span>*</span></label>
-          <textarea
-            v-model="message"
-            class="form-control"
-            id="message"
-            name="message"
-            placeholder="Insert message"
-            required
-          ></textarea>
-        </div>
-        <div class="mb-3 form-check">
-          <input
-            v-model="accepted"
-            type="checkbox"
-            class="form-check-input"
-            id="accept"
-            name="accept"
-            value="1"
-            required
-          />
-          <label class="form-check-label" for="accept"
-            ><span>*</span>I have read and accept the
-            <a href="#" target="_blank">terms of use</a> and the
-            <a href="#" target="_blank">privacy policy</a></label
-          >
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-        <div class="mt-3 mb-3">
-          All fields marked with<span> * </span>are mandatory
-        </div>
-      </form>
+      <div v-if="messageSent == false">
+        <form class="mb-3" method="POST" @submit.prevent="sendMessage()">
+          <div class="mb-3">
+            <label for="name" class="form-label">Name<span>*</span></label>
+            <input
+              v-model="name"
+              type="text"
+              class="form-control"
+              id="name"
+              name="name"
+              placeholder="Insert name"
+              required
+            />
+          </div>
+          <div class="mb-3">
+            <label for="email" class="form-label"
+              >Email address<span>*</span></label
+            >
+            <input
+              v-model="email"
+              type="email"
+              class="form-control"
+              id="email"
+              name="email"
+              required
+            />
+          </div>
+          <div class="mb-3">
+            <label for="message" class="form-label"
+              >Message<span>*</span></label
+            >
+            <textarea
+              v-model="message"
+              class="form-control"
+              id="message"
+              name="message"
+              placeholder="Insert message"
+              required
+            ></textarea>
+          </div>
+          <div class="mb-3 form-check">
+            <input
+              v-model="accepted"
+              type="checkbox"
+              class="form-check-input"
+              id="accept"
+              name="accept"
+              value="1"
+              required
+            />
+            <label class="form-check-label" for="accept"
+              ><span>*</span>I have read and accept the
+              <a href="#" target="_blank">terms of use</a> and the
+              <a href="#" target="_blank">privacy policy</a></label
+            >
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+          <div class="mt-3 mb-3">
+            All fields marked with<span> * </span>are mandatory
+          </div>
+        </form>
+      </div>
+      <div v-else>
+        <h2 class="text-center mt-1 mb-1">
+          The message has been successfully sent
+        </h2>
+      </div>
     </div>
   </main>
 </template>
